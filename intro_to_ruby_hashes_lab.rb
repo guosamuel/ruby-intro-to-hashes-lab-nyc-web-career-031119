b8c360a4129427923184601449c2a9c5efe80990
Samuel Guo <guosamuel1114@gmail.com>
2019-02-27 21:52:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,12 +55,14 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "$100",
 	      },
 	      pennsylvania_railroad: {
-	        "mortgage_value" => "200"
+	        "mortgage_value" => "$200"
 	      },
 	      b_and_o_railroad: {
-	        "mortgage_value" => "400"
+	        "mortgage_value" => "$400"
 	      },
-	      shortline: {}
+	      shortline: {
+	        "mortgage_value" => "$800"
+	      }
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
